e128de8dcdbce135b790409f62caf1960159ef6f
atharva <atarw@users.noreply.github.com>
2016-06-27 01:51:21 -0400

    solved autocomplete

--- dmoj/misc/HIDE_AND_SEEK.java
+++ dmoj/misc/HIDE_AND_SEEK.java
@@ -3,72 +3,174 @@ import java.util.*;
 
 public class HIDE_AND_SEEK {
   
-  static char [][] maze;
-  static int min = 100000;
+  static PrintWriter out = new PrintWriter (System.out);
+  static int min = Integer.MAX_VALUE;
+  static int [][] maze;
+  static int [][][] cache;
   
-  public static boolean legal (int x, int y) {
-    return x >= 0 && x < maze.length && y >= 0 && y < maze [0].length && maze [x][y] != 'X';
+  static int [] x, y;
+  
+  public static boolean valid (int t, int x, int y, int [][] maze, int [][][] cache, int curr) {
+    return x >= 0 && x < maze.length && y >= 0 && y < maze [0].length && maze [x][y] != 'X' && cache [t][x][y] > curr + 1;
   }
   
-  public static int solve (int x, int y, int t, int moves) {
-    if (t == 0) {
-      return moves;
+  public static void permute (List <Integer> path, int K) {
+    for (int k = K; k < path.size (); ++k) {
+      Collections.swap (path, k, K);
+      permute (path, k + 1);
+      Collections.swap (path, K, k);
     }
     
-    char tmp;
-    
-    if (legal (x + 1, y)) {
-      tmp = maze [x][y];
-      maze [x][y] = 'X';
-      min = Math.min (min, solve (x + 1, y, maze [x + 1][y] == 'H' ? t - 1 : t, moves + 1));
-      maze [x][y] = tmp;
+    if (K == path.size () - 1) {
+      int length = 0;
+      int loc = x.length - 1;
+      
+      for (int i = 0; i < path.size (); ++i) {
+        length += cache [loc][x [path.get (i)]][y [path.get (i)]];
+        loc = path.get (i);
+      }
+      
+      min = Math.min (length, min);
     }
-    
-    if (legal (x - 1, y)) {
-      tmp = maze [x][y];
-      maze [x][y] = 'X';
-      min = Math.min (min, solve (x - 1, y, maze [x - 1][y] == 'H' ? t - 1 : t, moves + 1));
-      maze [x][y] = tmp;
-    }
-    
-    if (legal (x, y + 1)) {
-      tmp = maze [x][y];
-      maze [x][y] = 'X';
-      min = Math.min (min, solve (x, y + 1, maze [x][y + 1] == 'H' ? t - 1 : t, moves + 1));
-      maze [x][y] = tmp;
-    }
-    
-    if (legal (x, y - 1)) {
-      tmp = maze [x][y];
-      maze [x][y] = 'X';
-      min = Math.min (min, solve (x, y - 1, maze [x][y - 1] == 'H' ? t - 1 : t, moves + 1));
-      maze [x][y] = tmp;
-    }
-    
-    return min;
   }
   
   public static void main (String [] args) throws IOException {
-    BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
-    String ln;
-    String [] t = in.readLine ().split (" ");
-    int N = Integer.parseInt (t [0]), M = Integer.parseInt (t [1]), T = Integer.parseInt (t [2]);
-    maze = new char [N][M];
-    int xs = 0, ys = 0;
+    INPUT in = new INPUT (System.in);
     
-    for (int n = 0; n < N; n++) {
-      ln = in.readLine ();
-      
-      for (int i = 0; i < M; i++) {
-        maze [n][i] = ln.charAt (i);
+    int N = in.iscan (), M = in.iscan (), T = in.iscan ();
+    
+    maze = new int [N][M];
+    
+    x = new int [T + 1]; y = new int [T + 1];//hider positions, last is griffy
+    int pos = 1;
+    
+    for (int n = 0; n < N; ++n) {
+      for (int m = 0; m < M; ++m) {
+        maze [n][m] = in.cscan ();
         
-        if (ln.charAt (i) == 'G') {
-          xs = n;
-          ys = i;
+        if (maze [n][m] == 'H') {
+          x [pos] = n; y [pos] = m; ++pos;
+        }       
+        else if (maze [n][m] == 'G') {
+          x [0] = n; x [0] = m;
+        }
+      }
+      
+      in.cscan ();
+    }
+    
+    Queue <P> queue;
+    P curr;
+    
+    cache = new int [T + 1][N][M];//from point specified by x [T + 1] and y [T + 1], min distance to point [N, M]
+    
+    for (int i = 0; i < x.length; ++i) {
+      for (int n = 0; n < N; ++n) {
+        for (int m = 0; m < M; ++m) {
+          cache [i][n][m] = Integer.MAX_VALUE;
         }
       }
     }
     
-    System.out.println (solve (xs, ys, T, 0));
+    for (int i = 0; i < x.length; ++i) {
+      cache [i][x [i]][y [i]] = 0;
+    }
+    
+    for (int i = 0; i < x.length; ++i) {
+      queue = new ArrayDeque <P> ();
+      curr = new P (x [i], y [i]);
+      queue.offer (curr);
+      
+      while (!queue.isEmpty ()) {
+        curr = queue.poll ();
+        
+        if (valid (i, curr.x - 1, curr.y, maze, cache, cache [i][curr.x][curr.y])) {
+          cache [i][curr.x - 1][curr.y] = cache [i][curr.x][curr.y] + 1;
+          queue.offer (new P (curr.x - 1, curr.y));
+        }
+        
+        if (valid (i, curr.x + 1, curr.y, maze, cache, cache [i][curr.x][curr.y])) {
+          cache [i][curr.x + 1][curr.y] = cache [i][curr.x][curr.y] + 1;
+          queue.offer (new P (curr.x + 1, curr.y));
+        }
+        
+        if (valid (i, curr.x, curr.y - 1, maze, cache, cache [i][curr.x][curr.y])) {
+          cache [i][curr.x][curr.y - 1] = cache [i][curr.x][curr.y] + 1;
+          queue.offer (new P (curr.x, curr.y - 1));
+        }
+        
+        if (valid (i, curr.x, curr.y + 1, maze, cache, cache [i][curr.x][curr.y])) {
+          cache [i][curr.x][curr.y + 1] = cache [i][curr.x][curr.y] + 1;
+          queue.offer (new P (curr.x, curr.y + 1));
+        }
+      }
+    }
+    
+    List <Integer> path = new ArrayList <Integer> ();
+    
+    for (int i = 0; i < T; ++i) {
+      path.add (i);
+    }
+    
+    permute (path, 0);
+    
+    out.print (min); out.close ();
+  }
+  
+  private static class INPUT {
+    
+    private InputStream stream;
+    private byte [] buf = new byte [1024];
+    private int curChar, numChars;
+    
+    public INPUT (InputStream stream) {
+      this.stream = stream;
+    }
+    
+    public int cscan () throws IOException {
+      if (curChar >= numChars) {
+        curChar = 0;
+        numChars = stream.read (buf);
+        
+        if (numChars <= 0) return -1;
+      }
+      
+      return buf [curChar++];
+    }
+    
+    public int iscan () throws IOException {
+      int c = cscan (), sgn = 1;
+      while (space (c)) c = cscan ();
+      
+      if (c == '-') {
+        sgn = -1;
+        c = cscan ();
+      }
+      
+      int res = 0;
+      
+      do
+      {
+        res *= 10;
+        res += c - '0';
+        
+        c = cscan ();
+      }
+      while (!space (c));
+      
+      return res * sgn;
+    }
+    
+    public boolean space (int c) {
+      return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+  }
+}
+
+class P {
+  int x, y;
+  
+  public P (int x, int y) {
+    this.x = x; this.y = y;
   }
 }
---
+++ fhc/FHC_15_AUTOCOMPLETE.java
@@ -0,0 +1,122 @@
+import java.io.*;
+import java.util.*;
+
+public class FHC_15_AUTOCOMPLETE {
+  
+  static int [][] trie;
+  static int insertionPoint;
+  
+  //returns longest prefix already in array
+  public static int insert (String word) {
+    int curr = 1, pos, length = 0;
+    boolean exists = true;//previously exists
+    
+    for (int i = 0; i < word.length (); ++i) {
+      pos = word.charAt (i) - 'a';
+      
+      if (trie [curr][pos] == 0) {//no child node at this position
+        trie [curr][pos] = ++insertionPoint;
+        exists = false;
+      }
+      else {
+        ++length;
+      }
+      
+      curr = trie [curr][pos];//set current node to child node
+    }
+    
+    return length + (exists ? 0 : 1);
+  }
+  
+  public static void main (String [] args) throws IOException {
+    INPUT in = new INPUT (System.in);
+    PrintWriter out = new PrintWriter (System.out);
+    
+    int T = in.iscan ();
+    int N, total;
+    
+    for (int t = 1; t <= T; ++t) {
+      trie = new int [1000001][26];//1000001 is the max number of nodes in trie, 26 is the max number of children of each node, returns address of node underneath
+      
+      System.gc ();//hopefully this works...
+      
+      insertionPoint = 1;
+      
+      N = in.iscan (); total = 0;
+      
+      while (N --> 0) {
+        total += insert (in.sscan ());
+      }
+      
+      out.print ("Case #"); out.print (t); out.print (": "); out.println (total);
+    }
+    
+    //trie = null; System.gc ();
+    out.close ();
+  }
+  
+  private static class INPUT {
+    
+    private InputStream stream;
+    private byte [] buf = new byte [1024];
+    private int curChar, numChars;
+    
+    public INPUT (InputStream stream) {
+      this.stream = stream;
+    }
+    
+    public int cscan () throws IOException {
+      if (curChar >= numChars) {
+        curChar = 0;
+        numChars = stream.read (buf);
+        
+        if (numChars <= 0) return -1;
+      }
+      
+      return buf [curChar++];
+    }
+    
+    public int iscan () throws IOException {
+      int c = cscan (), sgn = 1;
+      while (space (c)) c = cscan ();
+      
+      if (c == '-') {
+        sgn = -1;
+        c = cscan ();
+      }
+      
+      int res = 0;
+      
+      do
+      {
+        res *= 10;
+        res += c - '0';
+        
+        c = cscan ();
+      }
+      while (!space (c));
+      
+      return res * sgn;
+    }
+    
+    public String sscan () throws IOException {
+      int c = cscan ();
+      while (space (c)) c = cscan();
+      
+      StringBuilder res = new StringBuilder ();
+      
+      do
+      {
+        res.appendCodePoint (c);
+        c = cscan ();
+      }
+      while (!space (c));
+      
+      return res.toString ();
+    }
+    
+    public boolean space (int c) {
+      return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+  }
+}

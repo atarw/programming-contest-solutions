695d3ac9a0e765cd489c1f49f6b3224ccbe8b1ca
atharva <atarw@users.noreply.github.com>
2016-07-01 17:35:32 -0400

    solved dmopc problem

---
+++ misc/WORD_ORDERING.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class WORD_ORDERING {
+	
+	static String order;
+	
+	public static void main (String [] t) throws IOException {
+		BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
+		PrintWriter out = new PrintWriter (System.out);
+
+		List <String> list =  new ArrayList <String> ();
+		order = in.readLine (); order += order.toUpperCase ();
+		
+		int N = Integer.parseInt (in.readLine ());
+		
+		for (int n = 0; n < N; n++) {
+			list.add (in.readLine ());
+		}
+		
+		Collections.sort (list, new Comparator <String> () {
+			public int compare (String o1, String o2) {
+				int pos1, pos2;
+				
+				for (int i = 0; i < Math.min (o1.length (), o2.length ()); i++) {
+					pos1 = order.indexOf (o1.charAt (i)); pos2 = order.indexOf (o2.charAt (i));
+					
+					if (pos1 > pos2) {
+						return 1;
+					}
+					else if (pos1 < pos2) {
+						return -1;
+					}
+				}
+				
+				if (o1.length () > o2.length ()) {
+					return 1;
+				}
+				else if (o1.length () < o2.length ()) {
+					return -1;
+				}
+				else {
+					return 0;
+				}
+			}
+		});
+		
+		for (String i : list) {
+			out.println (i);
+		}
+		
+		out.close ();
+	}
+}

29ddd68f91d03ea422b2d04c538d4bddfdc105f7
atharva <atarw@users.noreply.github.com>
2016-09-20 19:59:54 -0400

    upload of bts solutions (p8 is incomplete)

---
+++ dmoj/bts/BTS_16_P4_SCREENSAVER.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class BTS_16_P4_SCREENSAVER {
+	public static void main (String [] t) throws IOException {
+		BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
+		PrintWriter out = new PrintWriter (System.out);
+
+		t = in.readLine ().split (" ");
+		int N = Integer.parseInt (t [0]), M = Integer.parseInt (t [1]), T = Integer.parseInt (t [2]);
+		char [][] maze = new char [M][N];
+		
+		int sx = -1, sy = -1;
+		
+		for (int m = 0; m < M; ++m) {
+			String ln = in.readLine ();
+			
+			for (int n = 0; n < N; ++n) {
+				maze [m][n] = ln.charAt (n);
+				
+				if (maze [m][n] == 'O') {
+					sx = m; sy = n;
+				}
+			}
+		}
+		
+		int x = 1;
+		int [] dir = {0, 1};
+		
+		for (; x <= T; ++x) {
+			sx += dir [0]; sy += dir [1];
+			
+			if (!(0 <= sx && sx < M && 0 <= sy && sy < N)) {
+				break;
+			}
+			
+			if (maze [sx][sy] == '.') {
+				continue;
+			}
+			
+			if (maze [sx][sy] == '-') {
+				if (dir [0] == 0) {// not going up/down
+					continue;
+				}
+				else {
+					dir = new int [] {-dir [0], 0};
+					maze [sx][sy] = '|';
+				}
+			}
+			else if (maze [sx][sy] == '|') {
+				if (dir [0] != 0) {// going up
+					continue;
+				}
+				else {
+					dir = new int [] {0, -dir [1]};
+					maze [sx][sy] = '-';
+				}
+			}
+			else if (maze [sx][sy] == '/') {
+				dir = new int [] {-dir [1], -dir [0]};
+				maze [sx][sy] = '\\';
+			}
+			else if (maze [sx][sy] == '\\') {
+				dir = new int [] {dir [1], dir [0]};
+				maze [sx][sy] = '/';
+			}
+		}
+		
+		if (0 <= sx && sx < M && 0 <= sy && sy < N) {
+			out.print ("The observer stays within the grid.");
+		}
+		else {
+			out.print ("The observer leaves the grid after " + x + " tick(s).");
+		}
+		
+		out.close ();
+	}
+}
---
+++ dmoj/bts/BTS_16_P8_TEXTBOOKS.java
@@ -0,0 +1,114 @@
+import java.io.*;
+import java.util.*;
+
+public class BTS_16_P8_TEXTBOOKS {
+  
+  public static void main (String [] t) throws IOException {
+    INPUT in = new INPUT (System.in);
+    PrintWriter out = new PrintWriter (System.out);
+    
+    int N = in.iscan ();
+    int [] s = new int [N], e = new int [N], w = new int [N];
+    
+    long used = 0L;
+    
+    NavigableSet <Integer> unique = new TreeSet <Integer> ();
+    
+    for (int n = 0; n < N; ++n) {
+      s [n] = in.iscan () - 1; int l = in.iscan (); e [n] = s [n] + l; w [n] = in.iscan ();
+      unique.add (s [n]); unique.add (e [n]);
+      used += (long) (w [n]) * (long) (l);
+    }
+    
+    Map <Integer, Integer> cc = new HashMap <Integer, Integer> ();
+    
+    for (int p : unique) {
+      cc.put (p, cc.size ());
+    }
+    
+    long [] arr = new long [cc.size ()];
+    
+    for (int n = 0; n < N; ++n) {
+      long max = -1;
+      
+      for (int b = cc.get (s [n]); b < cc.get (e [n]); ++b) {
+        max = Math.max (max, arr [b]);
+      }
+      
+      for (int a = cc.get (s [n]); a < cc.get (e [n]); ++a) {
+        arr [a] = w [n] + max;
+      }
+    }
+    
+    long shade = 0L;
+    
+    int f = unique.first ();
+    int n = 1;
+    
+    for (int p : unique) {
+      if (p == f) continue;
+      
+      shade += arr [n - 1] * (p - f);
+      f = p;
+      ++n;
+    }
+    
+    out.print ((shade - used) % 1000000007L);
+    out.close ();
+  }
+}
+
+class INPUT {
+  
+  private InputStream stream;
+  private byte [] buf = new byte [1024];
+  private int curChar, numChars;
+  
+  public INPUT (InputStream stream) {
+    this.stream = stream;
+  }
+  
+  public static int fast_pow (int b, int x) {
+    if (x == 0) return 1;
+    if (x == 1) return b;
+    if (x % 2 == 0) return fast_pow (b * b, x / 2);
+    
+    return b * fast_pow (b * b, x / 2);
+  }
+  
+  public int cscan () throws IOException {
+    if (curChar >= numChars) {
+      curChar = 0;
+      numChars = stream.read (buf);
+    }
+    
+    return buf [curChar++];
+  }
+  
+  public int iscan () throws IOException {
+    int c = cscan (), sgn = 1;
+    while (space (c)) c = cscan ();
+    
+    if (c == '-') {
+      sgn = -1;
+      c = cscan ();
+    }
+    
+    int res = 0;
+    
+    do
+    {
+      res = (res << 1) + (res << 3);
+      res += c - '0';
+      
+      c = cscan ();
+    }
+    while (!space (c));
+    
+    return res * sgn;
+  }
+  
+  public boolean space (int c) {
+    return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+  }
+}

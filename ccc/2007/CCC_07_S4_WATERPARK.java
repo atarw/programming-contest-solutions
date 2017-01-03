aa70edffafaab390fb80dd22ca0354487f383088
atharva <atarw@users.noreply.github.com>
2017-01-03 18:38:20 -0500

    re solved ccc problem

--- ccc/2007/CCC_07_S4_WATERPARK.java
+++ ccc/2007/CCC_07_S4_WATERPARK.java
@@ -2,42 +2,191 @@ import java.io.*;
 import java.util.*;
 
 public class CCC_07_S4_WATERPARK {
-  
-  static List <Integer> [] list;
-  static int [] caché;
-  
-  public static int ways (int curr) {
-    if (caché [curr] == 0 && list [curr] != null) {
-      for (int i = 0; i < list [curr].size (); i++) {
-        caché [curr] += ways (list [curr].get (i));
-      }
-    }
-    
-    return caché [curr];
-  }
-  
-  public static void main (String [] t) throws IOException {
-    BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
-    int N = Integer.parseInt (in.readLine ()), x;
-    list = new ArrayList [N]; caché = new int [N];
-    
-    while (true) {
-      t = in.readLine ().split (" ");
-      
-      if (t [0].equals ("0")) {
-        break;
-      }
-      
-      x = Integer.parseInt (t [0]) - 1;
-      
-      if (list [x] == null) {
-        list [x] = new ArrayList <Integer> ();
-      }
-      
-      list [x].add (Integer.parseInt (t [1]) - 1);
-    }
-    
-    caché [N - 1] = 1;
-    System.out.print (ways (0));
-  }
+	
+	static List <Integer> [] list;
+	static int [] dp;
+	
+	public static int solve (int u) {
+		if (dp [u] != 0)
+			return dp [u];
+		
+		if (u == list.length - 1)
+			return dp [u] = 1;
+		
+		for (int v : list [u])
+			dp [u] += solve (v);
+		
+		return dp [u];
+	}
+	
+	public static void main (String [] t) throws IOException {
+		INPUT in = new INPUT (System.in);
+		PrintWriter out = new PrintWriter (System.out);
+
+		int N = in.iscan ();
+		list = new ArrayList [N];
+		dp = new int [N];
+		
+		for (int n = 0; n < N; ++n)
+			list [n] = new ArrayList <Integer> ();
+		
+		int x, y;
+		
+		while (true) {
+			x = in.iscan () - 1; y = in.iscan () - 1;
+			
+			if (x == -1)
+				break;
+			
+			list [x].add (y);
+		}
+		
+		out.println (solve (0));
+		out.close ();
+	}
+
+	private static class INPUT {
+		
+		private InputStream stream;
+		private byte [] buf = new byte [1024];
+		private int curChar, numChars;
+		
+		public INPUT (InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public INPUT (String file) throws IOException {
+			this.stream = new FileInputStream (file);
+		}
+		
+		public static int fast_pow (int b, int x) {
+			if (x == 0) return 1;
+			if (x == 1) return b;
+			if (x % 2 == 0) return fast_pow (b * b, x / 2);
+			
+			return b * fast_pow (b * b, x / 2);
+		}
+		
+		public int cscan () throws IOException {
+			//if (numChars == -1) throw new InputMismatchException ();
+			
+			if (curChar >= numChars) {
+				curChar = 0;
+				numChars = stream.read (buf);
+				
+				//if (numChars <= 0) return -1;
+			}
+			
+			return buf [curChar++];
+		}
+		
+		public int iscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			int res = 0;
+			
+			do
+			{
+				//if (c < '0' || c > '9') throw new InputMismatchException ();
+				
+				res = (res << 1) + (res << 3);
+				//res *= 10;
+				res += c - '0';
+				
+				c = cscan ();
+			}
+			while (!space (c));
+			
+			return res * sgn;
+		}
+		
+		public String sscan () throws IOException {
+			int c = cscan ();
+			while (space (c)) c = cscan();
+			
+			StringBuilder res = new StringBuilder();
+			
+			do
+			{
+				res.appendCodePoint (c);
+				c = cscan ();
+			}
+			while (!space (c));
+			
+			return res.toString ();
+		}
+		
+		public double dscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			double res = 0;
+			
+			while (!space (c) && c != '.') {
+				if (c == 'e' || c == 'E') return res * fast_pow (10, iscan ()); /*Math.pow (10, iscan ());*/
+				//if (c < '0' || c > '9') throw new InputMismatchException ();
+				
+				//res = (res << 1) + (res << 3);
+				res *= 10;
+				res += c - '0';
+				c = cscan ();
+			}
+			
+			if (c == '.') {
+				c = cscan ();
+				double m = 1;
+				
+				while (!space (c)) {
+					if (c == 'e' || c == 'E') return res * fast_pow (10, iscan ()); /*Math.pow (10, iscan ());*/
+					//if (c < '0' || c > '9') throw new InputMismatchException ();
+					
+					m /= 10;
+					res += (c - '0') * m;
+					c = cscan ();
+				}
+			}
+			
+			return res * sgn;
+		}
+		
+		public long lscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			long res = 0;
+			
+			do {
+				//if (c < '0' || c > '9') throw new InputMismatchException();
+				
+				res = (res << 1) + (res << 3);
+				//res *= 10;
+				res += c - '0';
+				c = cscan ();
+				
+			}
+			while (!space (c));
+			
+			return res * sgn;
+		}
+		
+		public boolean space (int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	}
 }

2f5b493c8458668b1c026772302eb793eac9a998
atharva <atarw@users.noreply.github.com>
2017-01-16 17:00:12 -0500

    mnyc problems

---
+++ dmoj/mnyc/MNYC_17_P2_ASCII_ART_II.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class MNYC_17_P2_ASCII_ART_II {
+	public static void main (String [] t) throws IOException {
+		BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
+		PrintWriter out = new PrintWriter (System.out);
+
+		t = in.readLine ().split (" ");
+		int C = Integer.parseInt (t [0]), R = Integer.parseInt (t [1]);
+		
+		Map <Character, Integer> map = new HashMap <Character, Integer> ();
+		char ch;
+		
+		for (int r = 0; r < R; ++r) {
+			for (int c = 0; c < C; ++c) {
+				ch = (char) in.read ();
+				
+				if (!Character.isWhitespace (ch)) {
+					if (!map.containsKey (ch))
+						map.put (ch, 0);
+					map.put (ch, map.get (ch) + 1);
+				}
+			}
+			in.read ();
+		}
+		
+		int sum = 0;
+		
+		for (char k : map.keySet ()) {
+			sum += map.get (k);
+			
+			if (k != '.')
+				++sum;
+		}
+		
+		out.println (sum);
+		out.close ();
+	}
+}
---
+++ dmoj/mnyc/MNYC_17_P3_HURONTARIO.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class MNYC_17_P3_HURONTARIO {
+	public static void main (String [] t) throws IOException {
+		BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
+		PrintWriter out = new PrintWriter (System.out);
+		
+		t = in.readLine ().split (" ");
+		
+		String s1 = t [0], s2 = t [1];
+		int m = 0;
+		
+		for (int i = 0, j = 0; i < s1.length () && j < s2.length ();) {
+			if (i + s2.length () < s1.length ()) {
+				++i;
+				continue;
+			}
+			
+			if (s1.charAt (i) == s2.charAt (j)) {
+				while (s1.charAt (i) == s2.charAt (j)) {
+					++i; ++j; ++m;
+					
+					if (i == s1.length () || j == s2.length ())
+						break;
+				}
+			}
+			else {
+				++i;
+				
+				if (i == s1.length ()) {
+					m = 0;
+				}
+			}
+		}
+		
+		out.print (s1);
+		
+		if (m == 0) {
+			out.print (s2);
+		}
+		else {
+			for (int k = m; k < s2.length (); ++k)
+				out.print (s2.charAt (k));
+		}
+		
+		out.close ();
+	}
+}

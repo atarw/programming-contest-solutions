00b1088f50f34e3921f6bd98afe49234f65fa9ec
atharva <atarw@users.noreply.github.com>
2017-01-16 17:01:53 -0500

    added qr fhc problems

---
+++ fhc/FHC_17_P1_PROGRESS_PIE.java
@@ -0,0 +1,188 @@
+import java.io.*;
+import java.util.*;
+
+public class FHC_17_P1_PROGRESS_PIE {
+	
+	public static int dist (int x1, int y1, int x2, int y2) {
+		return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+	}
+	
+	public static void main (String [] t) throws IOException {
+		INPUT in = new INPUT (/*System.in*/"progress_pie2.txt");
+		PrintWriter out = new PrintWriter (/*System.out*/new File ("siavash.out"));
+
+		int T = in.iscan ();
+		
+		for (int tt = 1, p, x, y; tt <= T; ++tt) {
+			p = in.iscan (); x = in.iscan (); y = in.iscan ();
+			int dist_from_origin = dist (50, 50, x, y);
+			double angle = Math.toDegrees (Math.acos ((y - 50) / Math.sqrt (dist_from_origin)));
+			
+			if (x >= 50) { // Q1 or Q4
+				//System.out.print ((y >= 50 ? "Q1" : "Q4") + " ");
+			}
+			else { // Q2 or Q3
+				//System.out.print ((y >= 50 ? "Q2 " : "Q3") + " ");
+				angle = 360 - angle;
+			}
+						
+			//System.out.println ("DIST_FROM_ORIGIN: " + dist_from_origin + " ANGLE: " + angle);
+			
+			out.print ("Case #" + tt + ": ");
+			
+			if (p != 0 && (dist_from_origin == 0 || dist_from_origin <= 50 * 50 && angle <= p * 3.6)) {
+				out.println ("black");
+			}
+			else {
+				out.println ("white");
+			}
+		}
+
+		out.close ();
+	}
+
+	private static class INPUT {
+		
+		private InputStream stream;
+		private byte [] buf = new byte [1024];
+		private int curChar, numChars;
+		
+		public INPUT (InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public INPUT (String file) throws IOException {
+			this.stream = new FileInputStream (file);
+		}
+		
+		public static int fast_pow (int b, int x) {
+			if (x == 0) return 1;
+			if (x == 1) return b;
+			if (x % 2 == 0) return fast_pow (b * b, x / 2);
+			
+			return b * fast_pow (b * b, x / 2);
+		}
+		
+		public int cscan () throws IOException {
+			//if (numChars == -1) throw new InputMismatchException ();
+			
+			if (curChar >= numChars) {
+				curChar = 0;
+				numChars = stream.read (buf);
+				
+				//if (numChars <= 0) return -1;
+			}
+			
+			return buf [curChar++];
+		}
+		
+		public int iscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			int res = 0;
+			
+			do
+			{
+				//if (c < '0' || c > '9') throw new InputMismatchException ();
+				
+				res = (res << 1) + (res << 3);
+				//res *= 10;
+				res += c - '0';
+				
+				c = cscan ();
+			}
+			while (!space (c));
+			
+			return res * sgn;
+		}
+		
+		public String sscan () throws IOException {
+			int c = cscan ();
+			while (space (c)) c = cscan();
+			
+			StringBuilder res = new StringBuilder();
+			
+			do
+			{
+				res.appendCodePoint (c);
+				c = cscan ();
+			}
+			while (!space (c));
+			
+			return res.toString ();
+		}
+		
+		public double dscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			double res = 0;
+			
+			while (!space (c) && c != '.') {
+				if (c == 'e' || c == 'E') return res * fast_pow (10, iscan ()); /*Math.pow (10, iscan ());*/
+				//if (c < '0' || c > '9') throw new InputMismatchException ();
+				
+				//res = (res << 1) + (res << 3);
+				res *= 10;
+				res += c - '0';
+				c = cscan ();
+			}
+			
+			if (c == '.') {
+				c = cscan ();
+				double m = 1;
+				
+				while (!space (c)) {
+					if (c == 'e' || c == 'E') return res * fast_pow (10, iscan ()); /*Math.pow (10, iscan ());*/
+					//if (c < '0' || c > '9') throw new InputMismatchException ();
+					
+					m /= 10;
+					res += (c - '0') * m;
+					c = cscan ();
+				}
+			}
+			
+			return res * sgn;
+		}
+		
+		public long lscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			long res = 0;
+			
+			do {
+				//if (c < '0' || c > '9') throw new InputMismatchException();
+				
+				res = (res << 1) + (res << 3);
+				//res *= 10;
+				res += c - '0';
+				c = cscan ();
+				
+			}
+			while (!space (c));
+			
+			return res * sgn;
+		}
+		
+		public boolean space (int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	}
+}

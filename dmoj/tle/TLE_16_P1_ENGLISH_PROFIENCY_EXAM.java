4f993f999aeb57d3066518f057ae28cbc7bcc77b
atharva <atarw@users.noreply.github.com>
2017-01-22 16:55:48 -0500

    added tle sol

---
+++ dmoj/tle/TLE_16_P1_ENGLISH_PROFIENCY_EXAM.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class TLE_16_P1_ENGLISH_PROFIENCY_EXAM {
+	public static void main (String [] t) throws IOException {
+		BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
+		PrintWriter out = new PrintWriter (System.out);
+
+		t = in.readLine ().split (" ");
+		boolean read = true;
+		
+		for (int n = 0; n < t.length; ++n) {
+			int v = 0, c = 0;
+			
+			if (t [n].length () == 1 && !(t [n].charAt (0) == 'a' || t [n].charAt (0) == 'e' || t [n].charAt (0) == 'i' || t [n].charAt (0) == 'o' || t [n].charAt (0) == 'u')) {
+				read = false;
+				break;
+			}
+			
+			for (int i = 0; i < t [n].length (); ++i) {
+				if (t [n].charAt (i) == 'a' || t [n].charAt (i) == 'e' || t [n].charAt (i) == 'i' || t [n].charAt (i) == 'o' || t [n].charAt (i) == 'u')
+					++v;
+				else
+					++c;
+			}
+			
+			if (!(v == c || v + 1 == c || c + 1 == v)) {
+				read = false;
+				break;
+			}
+		}
+		
+		out.println ((read ? "" : "not ") + "readable");
+		out.close ();
+	}
+}
---
+++ dmoj/tle/TLE_16_P4_ENGINEERING_TEST.java
@@ -0,0 +1,272 @@
+import java.io.*;
+import java.util.*;
+
+public class TLE_16_P4_ENGINEERING_TEST {
+	
+	static long K;
+	static int N;
+	static long [] arr;
+	
+	public static boolean possible (int height) {
+		long [][] grid = new long [height + 1][height + 1];
+		boolean [] used = new boolean [N];
+		
+		int total = (height * (height + 1)) / 2;
+		boolean poss = true;
+		
+		if (total > arr.length) {
+			poss = false;
+		}
+		else {
+			out: for (int h = 1; h <= height; ++h) {
+				for (int c = 1; c <= h; ++c) {
+					grid [h][c] = (grid [h - 1][c] + grid [h - 1][c - 1]) / 2L;
+					
+					int n = 0;
+					
+					for (; n < N; ++n) {
+						if (!used [n] && arr [n] >= grid [h][c]) {
+							used [n] = true;
+							break;
+						}
+					}
+					
+					if (n == N) {
+						poss = false;
+						break out;
+					}
+					
+					grid [h][c] += K;
+				}
+			}
+		}
+			
+		return poss;
+	}
+	
+	public static void main (String [] t) throws IOException {
+		BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
+		PrintWriter out = new PrintWriter (System.out);
+
+		K = Long.parseLong (in.readLine ()) * 2L;
+		N = Integer.parseInt (in.readLine ());
+		
+		arr = new long [N];
+		
+		t = in.readLine ().split (" ");
+		
+		for (int n = 0; n < N; ++n)
+			arr [n] = Long.parseLong (t [n]) * 2L;
+		
+		Arrays.sort (arr);
+		
+		for (int p = 70; p >= 1; --p) {
+			if (possible (p)) {
+				out.println (p);
+				break;	
+			}
+		}
+		
+		out.close ();
+	}
+
+	private static class INPUT {
+		
+		private InputStream stream;
+		private byte [] buf = new byte [1024];
+		private int curChar, numChars;
+		
+		public INPUT (InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public INPUT (String file) throws IOException {
+			this.stream = new FileInputStream (file);
+		}
+
+		public int cscan () throws IOException {
+			if (curChar >= numChars) {
+				curChar = 0;
+				numChars = stream.read (buf);
+			}
+			
+			return buf [curChar++];
+		}
+		
+		public int iscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			int res = 0;
+			
+			do {
+				res = (res << 1) + (res << 3);
+				res += c - '0';
+				c = cscan ();
+			}
+			while (!space (c));
+			
+			return res * sgn;
+		}
+		
+		public String sscan () throws IOException {
+			int c = cscan ();
+			while (space (c)) c = cscan();
+			
+			StringBuilder res = new StringBuilder();
+			
+			do
+			{
+				res.appendCodePoint (c);
+				c = cscan ();
+			}
+			while (!space (c));
+			
+			return res.toString ();
+		}
+		
+		public double dscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			double res = 0;
+			
+			while (!space (c) && c != '.') {
+				if (c == 'e' || c == 'E') return res * UTILITIES.fast_pow (10, iscan ());
+				res *= 10;
+				res += c - '0';
+				c = cscan ();
+			}
+			
+			if (c == '.') {
+				c = cscan ();
+				double m = 1;
+				
+				while (!space (c)) {
+					if (c == 'e' || c == 'E') return res * UTILITIES.fast_pow (10, iscan ());
+					
+					m /= 10;
+					res += (c - '0') * m;
+					c = cscan ();
+				}
+			}
+			
+			return res * sgn;
+		}
+		
+		public long lscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			long res = 0;
+			
+			do {
+				res = (res << 1) + (res << 3);
+				res += c - '0';
+				c = cscan ();
+				
+			}
+			while (!space (c));
+			
+			return res * sgn;
+		}
+		
+		public boolean space (int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	}
+	
+	public static class UTILITIES {
+		
+		static final double EPS = 10e-6;
+		
+		public static int lower_bound (int [] arr, int x) {
+			int low = 0, high = arr.length, mid = -1;
+			
+			while (low < high) {
+				mid = (low + high) / 2;
+				
+				if (arr [mid] >= x)
+					high = mid;
+				else
+					low = mid + 1;
+			}
+			
+			return low;
+		}
+		
+		public static int upper_bound (int [] arr, int x) {
+			int low = 0, high = arr.length, mid = -1;
+			
+			while (low < high) {
+				mid = (low + high) / 2;
+				
+				if (arr [mid] > x)
+					high = mid;
+				else
+					low = mid + 1;
+			}
+			
+			return low;
+		}
+		
+		public static int gcd (int a, int b) {
+			return b == 0 ? a : gcd (b, a % b);
+		}
+		
+		public static int lcm (int a, int b) {
+			return a * b / gcd (a, b);
+		}
+
+		public static int fast_pow_mod (int b, int x, int mod) {
+			if (x == 0) return 1;
+			if (x == 1) return b;
+			if (x % 2 == 0) return fast_pow_mod (b * b % mod, x / 2, mod) % mod;
+			
+			return b * fast_pow_mod (b * b % mod, x / 2, mod) % mod;
+		}
+
+		public static int fast_pow (int b, int x) {
+			if (x == 0) return 1;
+			if (x == 1) return b;
+			if (x % 2 == 0) return fast_pow (b * b, x / 2);
+			
+			return b * fast_pow (b * b, x / 2);
+		}
+		
+		public static long choose (long n, long k) {
+			k = Math.min (k, n - k);
+			long val = 1;
+			
+			for (int i = 0; i < k; ++i)
+				val = val * (n - i) / (i + 1);
+				
+			return val;
+		}
+		
+		public static long permute (int n, int k) {
+			if (n < k) return 0;
+			long val = 1;
+			
+			for (int i = 0; i < k; ++i)
+				val = (val * (n - i));
+				
+			return val;
+		}
+	}
+}

c2dd2ba9e26f5f729bce3483539e125ef317fe9c
atharva <atarw@users.noreply.github.com>
2017-03-11 20:13:37 -0500

    updated to use classical djikstras

--- ccc/2009/CCC_09_S4_SHOP_AND_SHIP.java
+++ ccc/2009/CCC_09_S4_SHOP_AND_SHIP.java
@@ -2,72 +2,254 @@ import java.io.*;
 import java.util.*;
 
 public class CCC_09_S4_SHOP_AND_SHIP {
-  
-  static int [] cache;
-  static short [][] matrix;
-  static short [] cities;
-  
-  public static void addEdge (int S, int E, short W) {
-    S--;
-    E--;
-    
-    if (matrix [S][E] == 0 || W < matrix [S][E]) {
-      matrix [S][E] = W;
-      matrix [E][S] = W;
-    }
-  }
-  
-  public static int traverse (int D) {
-    Queue <Integer> queue = new ArrayDeque <Integer> ();
-    boolean [] visited = new boolean [matrix.length];
-    
-    int curr, min = cities [D] == 0 ? Integer.MAX_VALUE : cities [D];
-    queue.offer (D);
-    
-    while (!queue.isEmpty ()) {
-      curr = queue.poll ();
-      
-      for (int i = 0; i < matrix.length; i++) {
-        if (matrix [curr][i] != 0 && (cache [i] == 0 || cache [i] > cache [curr] + matrix [curr][i])) {
-          cache [i] = cache [curr] + matrix [curr][i];
-          
-          if (cities [i] != 0) {
-            min = Math.min (min, cache [i] + cities [i]);
-          }
-          
-          if (min > cache [i] && !visited [i]) {
-            queue.offer (i);
-          }
-        }
-      }
-      
-      visited [curr] = true;
-    }
-    
-    return min;
-  }
-  
-  public static void main (String [] t) throws IOException {
-    BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
-    int N = Integer.parseInt (in.readLine ());
-    int T = Integer.parseInt (in.readLine ());
-    
-    cache = new int [N];
-    matrix = new short [N][N];
-    cities = new short [N];
-    
-    while (T --> 0) {
-      t = in.readLine ().split (" ");
-      addEdge (Integer.parseInt (t [0]), Integer.parseInt (t [1]), Short.parseShort (t [2]));
-    }
-    
-    int K = Integer.parseInt (in.readLine ());
-    
-    while (K --> 0) {
-      t = in.readLine ().split (" ");
-      cities [Integer.parseInt (t [0]) - 1] = Short.parseShort (t [1]);
-    }
-    
-    System.out.print (traverse (Integer.parseInt (in.readLine ()) - 1));
-  }
+	public static void main (String [] t) throws IOException {
+		INPUT in = new INPUT (System.in);
+		PrintWriter out = new PrintWriter (System.out);
+
+		int N = in.iscan (), M = in.iscan ();
+		int [][] matrix = new int [N][N];
+		int [] cost = new int [N], dist = new int [N];
+		boolean [] vis = new boolean [N];
+		
+		for (int n = 0; n < N; ++n)	
+			cost [n] = dist [n] = 1 << 30;
+		
+		for (int m = 0, a, b, w; m < M; ++m) {
+			a = in.iscan () - 1; b = in.iscan () - 1; w = in.iscan ();
+			matrix [a][b] = matrix [b][a] = matrix [a][b] == 0 ? w : Math.min (matrix [a][b], w);
+		}
+		
+		int K = in.iscan ();
+		
+		for (int k = 0; k < K; ++k)
+			cost [in.iscan () - 1] = in.iscan ();
+		
+		int D = in.iscan () - 1;
+		dist [D] = 0;
+		
+		int min;
+		
+		for (int n = 0; n < N; ++n) {
+			min = -1;
+			
+			for (int n2 = 0; n2 < N; ++n2)
+				if (!vis [n2] && (min == -1 || dist [n2] < dist [min]))
+					min = n2;
+			
+			vis [min] = true;
+			
+			for (int n2 = 0; n2 < N; ++n2)
+				if (matrix [min][n2] != 0)
+					dist [n2] = Math.min (dist [n2], dist [min] + matrix [min][n2]);
+		}
+		
+		min = 1 << 30;
+		
+		for (int n = 0; n < N; ++n)
+			if (cost [n] != 1 << 30)
+				min = Math.min (min, dist [n] + cost [n]);
+		
+		out.print (min);
+		out.close ();
+	}
+
+	private static class INPUT {
+		
+		private InputStream stream;
+		private byte [] buf = new byte [1024];
+		private int curChar, numChars;
+		
+		public INPUT (InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public INPUT (String file) throws IOException {
+			this.stream = new FileInputStream (file);
+		}
+
+		public int cscan () throws IOException {
+			if (curChar >= numChars) {
+				curChar = 0;
+				numChars = stream.read (buf);
+			}
+			
+			return buf [curChar++];
+		}
+		
+		public int iscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			int res = 0;
+			
+			do {
+				res = (res << 1) + (res << 3);
+				res += c - '0';
+				c = cscan ();
+			}
+			while (!space (c));
+			
+			return res * sgn;
+		}
+		
+		public String sscan () throws IOException {
+			int c = cscan ();
+			while (space (c)) c = cscan();
+			
+			StringBuilder res = new StringBuilder();
+			
+			do
+			{
+				res.appendCodePoint (c);
+				c = cscan ();
+			}
+			while (!space (c));
+			
+			return res.toString ();
+		}
+		
+		public double dscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			double res = 0;
+			
+			while (!space (c) && c != '.') {
+				if (c == 'e' || c == 'E') return res * UTILITIES.fast_pow (10, iscan ());
+				res *= 10;
+				res += c - '0';
+				c = cscan ();
+			}
+			
+			if (c == '.') {
+				c = cscan ();
+				double m = 1;
+				
+				while (!space (c)) {
+					if (c == 'e' || c == 'E') return res * UTILITIES.fast_pow (10, iscan ());
+					
+					m /= 10;
+					res += (c - '0') * m;
+					c = cscan ();
+				}
+			}
+			
+			return res * sgn;
+		}
+		
+		public long lscan () throws IOException {
+			int c = cscan (), sgn = 1;
+			while (space (c)) c = cscan ();
+			
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+			
+			long res = 0;
+			
+			do {
+				res = (res << 1) + (res << 3);
+				res += c - '0';
+				c = cscan ();
+				
+			}
+			while (!space (c));
+			
+			return res * sgn;
+		}
+		
+		public boolean space (int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	}
+	
+	public static class UTILITIES {
+		
+		static final double EPS = 10e-6;
+		
+		public static int lower_bound (int [] arr, int x) {
+			int low = 0, high = arr.length, mid = -1;
+			
+			while (low < high) {
+				mid = (low + high) / 2;
+				
+				if (arr [mid] >= x)
+					high = mid;
+				else
+					low = mid + 1;
+			}
+			
+			return low;
+		}
+		
+		public static int upper_bound (int [] arr, int x) {
+			int low = 0, high = arr.length, mid = -1;
+			
+			while (low < high) {
+				mid = (low + high) / 2;
+				
+				if (arr [mid] > x)
+					high = mid;
+				else
+					low = mid + 1;
+			}
+			
+			return low;
+		}
+		
+		public static int gcd (int a, int b) {
+			return b == 0 ? a : gcd (b, a % b);
+		}
+		
+		public static int lcm (int a, int b) {
+			return a * b / gcd (a, b);
+		}
+
+		public static int fast_pow_mod (int b, int x, int mod) {
+			if (x == 0) return 1;
+			if (x == 1) return b;
+			if (x % 2 == 0) return fast_pow_mod (b * b % mod, x / 2, mod) % mod;
+			
+			return b * fast_pow_mod (b * b % mod, x / 2, mod) % mod;
+		}
+
+		public static int fast_pow (int b, int x) {
+			if (x == 0) return 1;
+			if (x == 1) return b;
+			if (x % 2 == 0) return fast_pow (b * b, x / 2);
+			
+			return b * fast_pow (b * b, x / 2);
+		}
+		
+		public static long choose (long n, long k) {
+			k = Math.min (k, n - k);
+			long val = 1;
+			
+			for (int i = 0; i < k; ++i)
+				val = val * (n - i) / (i + 1);
+				
+			return val;
+		}
+		
+		public static long permute (int n, int k) {
+			if (n < k) return 0;
+			long val = 1;
+			
+			for (int i = 0; i < k; ++i)
+				val = (val * (n - i));
+				
+			return val;
+		}
+	}
 }

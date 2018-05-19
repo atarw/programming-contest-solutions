e150840c1a4ba5df8b46334a6dd88ab1faf645bd
atarw <atarw11@outlook.com>
2018-05-19 00:52:32 -0400

    cf problems

---
+++ codeforces/CODEFORCES_359_D.java
@@ -0,0 +1,302 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.NavigableSet;
+import java.util.PriorityQueue;
+import java.util.TreeSet;
+
+// atharva washimkar
+// May 19, 2018
+
+public class CODEFORCES_359_D {
+
+	public static void main (String[] t) throws IOException {
+		INPUT in = new INPUT (System.in);
+		PrintWriter out = new PrintWriter (System.out);
+
+		int N = in.iscan ();
+		int[] arr = new int[N];
+		PriorityQueue<Pair> q = new PriorityQueue<Pair> ();
+
+		for (int n = 0; n < N; ++n) {
+			arr[n] = in.iscan ();
+			q.offer (new Pair (n, arr[n]));
+		}
+
+		int cnt = 0, maxlen = 0;
+		boolean[] done = new boolean[N];
+		NavigableSet<Integer> idx = new TreeSet<Integer> ();
+
+		while (!q.isEmpty ()) {
+			Pair cur = q.poll ();
+
+			if (done[cur.i])
+				continue;
+
+			done[cur.i] = true;
+			int left = 0, right = 0;
+
+			// go left
+			for (int i = cur.i - 1; i >= 0 && arr[i] % cur.v == 0; --i) {
+				done[i] = true;
+				++left;
+			}
+
+			// go right
+			for (int i = cur.i + 1; i < N && arr[i] % cur.v == 0; ++i) {
+				done[i] = true;
+				++right;
+			}
+
+			int curlen = left + 1 + right;
+
+			if (curlen > maxlen) {
+				maxlen = curlen;
+				cnt = 1;
+				idx.clear ();
+				idx.add (cur.i - left + 1);
+			}
+			else if (curlen == maxlen) {
+				++cnt;
+				idx.add (cur.i - left + 1);
+			}
+		}
+
+		out.println (cnt + " " + (maxlen - 1));
+
+		for (int i : idx)
+			out.print (i + " ");
+
+		out.close ();
+	}
+
+	private static class Pair implements Comparable<Pair> {
+
+		int i, v;
+
+		public int compareTo (Pair p) {
+			if (Integer.compare (this.v, p.v) != 0)
+				return Integer.compare (this.v, p.v);
+
+			return Integer.compare (this.i, p.i);
+		}
+
+		public Pair (int i, int v) {
+			this.i = i;
+			this.v = v;
+		}
+	}
+
+	private static class INPUT {
+
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar, numChars;
+
+		public INPUT (InputStream stream) {
+			this.stream = stream;
+		}
+
+		public INPUT (String file) throws IOException {
+			this.stream = new FileInputStream (file);
+		}
+
+		public int cscan () throws IOException {
+			if (curChar >= numChars) {
+				curChar = 0;
+				numChars = stream.read (buf);
+			}
+
+			if (numChars == -1)
+				return numChars;
+
+			return buf[curChar++];
+		}
+
+		public int iscan () throws IOException {
+			int c = cscan (), sgn = 1;
+
+			while (space (c))
+				c = cscan ();
+
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+
+			int res = 0;
+
+			do {
+				res = (res << 1) + (res << 3);
+				res += c - '0';
+				c = cscan ();
+			}
+			while (!space (c));
+
+			return res * sgn;
+		}
+
+		public String sscan () throws IOException {
+			int c = cscan ();
+
+			while (space (c))
+				c = cscan ();
+
+			StringBuilder res = new StringBuilder ();
+
+			do {
+				res.appendCodePoint (c);
+				c = cscan ();
+			}
+			while (!space (c));
+
+			return res.toString ();
+		}
+
+		public double dscan () throws IOException {
+			int c = cscan (), sgn = 1;
+
+			while (space (c))
+				c = cscan ();
+
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+
+			double res = 0;
+
+			while (!space (c) && c != '.') {
+				if (c == 'e' || c == 'E')
+					return res * UTILITIES.fast_pow (10, iscan ());
+
+				res *= 10;
+				res += c - '0';
+				c = cscan ();
+			}
+
+			if (c == '.') {
+				c = cscan ();
+				double m = 1;
+
+				while (!space (c)) {
+					if (c == 'e' || c == 'E')
+						return res * UTILITIES.fast_pow (10, iscan ());
+
+					m /= 10;
+					res += (c - '0') * m;
+					c = cscan ();
+				}
+			}
+
+			return res * sgn;
+		}
+
+		public long lscan () throws IOException {
+			int c = cscan (), sgn = 1;
+
+			while (space (c))
+				c = cscan ();
+
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+
+			long res = 0;
+
+			do {
+				res = (res << 1) + (res << 3);
+				res += c - '0';
+				c = cscan ();
+			}
+			while (!space (c));
+
+			return res * sgn;
+		}
+
+		public boolean space (int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	}
+
+	public static class UTILITIES {
+
+		static final double EPS = 10e-6;
+
+		public static int lower_bound (int[] arr, int x) {
+			int low = 0, high = arr.length, mid = -1;
+
+			while (low < high) {
+				mid = (low + high) / 2;
+
+				if (arr[mid] >= x)
+					high = mid;
+				else
+					low = mid + 1;
+			}
+
+			return low;
+		}
+
+		public static int upper_bound (int[] arr, int x) {
+			int low = 0, high = arr.length, mid = -1;
+
+			while (low < high) {
+				mid = (low + high) / 2;
+
+				if (arr[mid] > x)
+					high = mid;
+				else
+					low = mid + 1;
+			}
+
+			return low;
+		}
+
+		public static int gcd (int a, int b) {
+			return b == 0 ? a : gcd (b, a % b);
+		}
+
+		public static int lcm (int a, int b) {
+			return a * b / gcd (a, b);
+		}
+
+		public static int fast_pow_mod (int b, int x, int mod) {
+			if (x == 0) return 1;
+			if (x == 1) return b;
+			if (x % 2 == 0) return fast_pow_mod (b * b % mod, x / 2, mod) % mod;
+
+			return b * fast_pow_mod (b * b % mod, x / 2, mod) % mod;
+		}
+
+		public static int fast_pow (int b, int x) {
+			if (x == 0) return 1;
+			if (x == 1) return b;
+			if (x % 2 == 0) return fast_pow (b * b, x / 2);
+
+			return b * fast_pow (b * b, x / 2);
+		}
+
+		public static long choose (long n, long k) {
+			k = Math.min (k, n - k);
+			long val = 1;
+
+			for (int i = 0; i < k; ++i)
+				val = val * (n - i) / (i + 1);
+
+			return val;
+		}
+
+		public static long permute (int n, int k) {
+			if (n < k) return 0;
+			long val = 1;
+
+			for (int i = 0; i < k; ++i)
+				val = (val * (n - i));
+
+			return val;
+		}
+	}
+}
---
+++ codeforces/CODEFORCES_404_C.java
@@ -0,0 +1,295 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+// atharva washimkar
+// May 19, 2018
+
+public class CODEFORCES_404_C {
+
+	public static void main (String[] t) throws IOException {
+		INPUT in = new INPUT (System.in);
+		PrintWriter out = new PrintWriter (System.out);
+
+		int N = in.iscan (), K = in.iscan ();
+		Queue<Integer>[] levels = new ArrayDeque[N];
+		Queue<Integer>[] levels2 = new ArrayDeque[N];
+
+		for (int n = 0; n < N; ++n) {
+			levels[n] = new ArrayDeque<Integer> ();
+			levels2[n] = new ArrayDeque<Integer> ();
+		}
+
+		for (int n = 0, l; n < N; ++n) {
+			l = in.iscan ();
+			levels[l].offer (n);
+			levels2[l].offer (n);
+		}
+
+		boolean good = (levels[0].size () == 1);
+		List<Edge> edges = new ArrayList<Edge> ();
+		int[] ind = new int[N];
+
+		for (int l = 1; l < N && good; ++l) {
+			for (int v : levels2[l]) {
+				boolean good2 = false;
+
+				while (!levels[l - 1].isEmpty () && !good2) {
+					int u = levels[l - 1].peek ();
+
+					if (ind[u] >= K)
+						levels[l - 1].poll ();
+					else {
+						edges.add (new Edge (u + 1, v + 1));
+						good2 = true;
+						++ind[u];
+						++ind[v];
+					}
+				}
+
+				if (!good2) {
+					good = false;
+					break;
+				}
+			}
+		}
+
+		if (good) {
+			out.println (edges.size ());
+
+			for (Edge e : edges)
+				out.println (e.u + " " + e.v);
+		}
+		else
+			out.print (-1);
+
+		out.close ();
+	}
+
+	private static class Edge {
+
+		int u, v;
+
+		public Edge (int u, int v) {
+			this.u = u;
+			this.v = v;
+		}
+	}
+
+	private static class INPUT {
+
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar, numChars;
+
+		public INPUT (InputStream stream) {
+			this.stream = stream;
+		}
+
+		public INPUT (String file) throws IOException {
+			this.stream = new FileInputStream (file);
+		}
+
+		public int cscan () throws IOException {
+			if (curChar >= numChars) {
+				curChar = 0;
+				numChars = stream.read (buf);
+			}
+
+			if (numChars == -1)
+				return numChars;
+
+			return buf[curChar++];
+		}
+
+		public int iscan () throws IOException {
+			int c = cscan (), sgn = 1;
+
+			while (space (c))
+				c = cscan ();
+
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+
+			int res = 0;
+
+			do {
+				res = (res << 1) + (res << 3);
+				res += c - '0';
+				c = cscan ();
+			}
+			while (!space (c));
+
+			return res * sgn;
+		}
+
+		public String sscan () throws IOException {
+			int c = cscan ();
+
+			while (space (c))
+				c = cscan ();
+
+			StringBuilder res = new StringBuilder ();
+
+			do {
+				res.appendCodePoint (c);
+				c = cscan ();
+			}
+			while (!space (c));
+
+			return res.toString ();
+		}
+
+		public double dscan () throws IOException {
+			int c = cscan (), sgn = 1;
+
+			while (space (c))
+				c = cscan ();
+
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+
+			double res = 0;
+
+			while (!space (c) && c != '.') {
+				if (c == 'e' || c == 'E')
+					return res * UTILITIES.fast_pow (10, iscan ());
+
+				res *= 10;
+				res += c - '0';
+				c = cscan ();
+			}
+
+			if (c == '.') {
+				c = cscan ();
+				double m = 1;
+
+				while (!space (c)) {
+					if (c == 'e' || c == 'E')
+						return res * UTILITIES.fast_pow (10, iscan ());
+
+					m /= 10;
+					res += (c - '0') * m;
+					c = cscan ();
+				}
+			}
+
+			return res * sgn;
+		}
+
+		public long lscan () throws IOException {
+			int c = cscan (), sgn = 1;
+
+			while (space (c))
+				c = cscan ();
+
+			if (c == '-') {
+				sgn = -1;
+				c = cscan ();
+			}
+
+			long res = 0;
+
+			do {
+				res = (res << 1) + (res << 3);
+				res += c - '0';
+				c = cscan ();
+			}
+			while (!space (c));
+
+			return res * sgn;
+		}
+
+		public boolean space (int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	}
+
+	public static class UTILITIES {
+
+		static final double EPS = 10e-6;
+
+		public static int lower_bound (int[] arr, int x) {
+			int low = 0, high = arr.length, mid = -1;
+
+			while (low < high) {
+				mid = (low + high) / 2;
+
+				if (arr[mid] >= x)
+					high = mid;
+				else
+					low = mid + 1;
+			}
+
+			return low;
+		}
+
+		public static int upper_bound (int[] arr, int x) {
+			int low = 0, high = arr.length, mid = -1;
+
+			while (low < high) {
+				mid = (low + high) / 2;
+
+				if (arr[mid] > x)
+					high = mid;
+				else
+					low = mid + 1;
+			}
+
+			return low;
+		}
+
+		public static int gcd (int a, int b) {
+			return b == 0 ? a : gcd (b, a % b);
+		}
+
+		public static int lcm (int a, int b) {
+			return a * b / gcd (a, b);
+		}
+
+		public static int fast_pow_mod (int b, int x, int mod) {
+			if (x == 0) return 1;
+			if (x == 1) return b;
+			if (x % 2 == 0) return fast_pow_mod (b * b % mod, x / 2, mod) % mod;
+
+			return b * fast_pow_mod (b * b % mod, x / 2, mod) % mod;
+		}
+
+		public static int fast_pow (int b, int x) {
+			if (x == 0) return 1;
+			if (x == 1) return b;
+			if (x % 2 == 0) return fast_pow (b * b, x / 2);
+
+			return b * fast_pow (b * b, x / 2);
+		}
+
+		public static long choose (long n, long k) {
+			k = Math.min (k, n - k);
+			long val = 1;
+
+			for (int i = 0; i < k; ++i)
+				val = val * (n - i) / (i + 1);
+
+			return val;
+		}
+
+		public static long permute (int n, int k) {
+			if (n < k) return 0;
+			long val = 1;
+
+			for (int i = 0; i < k; ++i)
+				val = (val * (n - i));
+
+			return val;
+		}
+	}
+}
